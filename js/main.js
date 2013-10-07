fceb4725c1d86950f801b2acead8cc62c4fdacc5
unknown <kkosorukov@atlant-inform.ru>
2013-10-07 16:16:43 +0400

    First Master Branch

---
+++ js/main.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function() {
+    var global = {
+        dateFields: [
+            '#birthday-cont',
+            '#document-givedate-cont',
+            '#search-date-cont'
+        ],
+        colorPickerFields: [
+            '.custom-color' // Маркировка анкет
+        ]
+    }
+
+    this.initFields = function() {
+        $(function () {
+            // Поля дат
+            for(var i = 0; i < global.dateFields.length; i++) {
+                $(global.dateFields[i]).datetimepicker();
+            }
+            // Маркировка анкет
+            for(var i = 0; i < global.colorPickerFields.length; i++) {
+                $(global.colorPickerFields[i]).colorpicker({
+                    format: 'hex'
+                });
+            }
+        });
+    };
+
+
+    this.initFields();
+});
