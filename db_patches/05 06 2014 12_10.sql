fdb8e1654f77d2954f04da024f7c13c3606a0ba7
Igor Khitrov <ihitrov@atlant-inform.ru>
2014-06-05 12:12:00 +0400

    1

---
+++ db_patches/05 06 2014 12_10.sql
@@ -0,0 +1,9 @@
+﻿-- Column: greeting_status
+
+-- ALTER TABLE mis.doctor_shedule_by_day DROP COLUMN greeting_status;
+
+ALTER TABLE mis.doctor_shedule_by_day ADD COLUMN greeting_status integer;
+ALTER TABLE mis.doctor_shedule_by_day ALTER COLUMN greeting_status SET DEFAULT 0;
+COMMENT ON COLUMN mis.doctor_shedule_by_day.greeting_status IS 'Статус приёма (да/нет)';
+
+UPDATE mis.doctor_shedule_by_day SET greeting_status=0
