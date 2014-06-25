23193cc0a9162e2c67606f67af9c87a8c5880213
Igor Khitrov <ihitrov@atlant-inform.ru>
2014-06-25 13:05:53 +0400

    (!)

--- db_patches/25 06 2014 12_55.sql
+++ db_patches/25 06 2014 12_55.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO mis.access_actions(
             name, "group", "accessKey")
-    VALUES ('Просмотр архива приёмов', 4, 'canViewGreetingArchive');
+    VALUES ('Просмотр результатов всех приёмов', 4, 'canViewGreetingArchive');
