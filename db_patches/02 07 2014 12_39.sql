e43ebac6ec874e71fa39929d97accd5de8ec6d9b
Igor Khitrov <ihitrov@atlant-inform.ru>
2014-07-02 17:10:03 +0400

    (!)

--- db_patches/02 07 2014 12_39.sql
+++ db_patches/02 07 2014 12_39.sql
@@ -2,5 +2,5 @@
 
 -- ALTER TABLE mis.cancelled_greetings DROP COLUMN comment;
 
-ALTER TABLE mis.cancelled_greetings ADD COLUMN comment text;
-COMMENT ON COLUMN mis.cancelled_greetings.comment IS 'Комментарий при записи';
+--ALTER TABLE mis.cancelled_greetings ADD COLUMN comment text;
+--COMMENT ON COLUMN mis.cancelled_greetings.comment IS 'Комментарий при записи';
