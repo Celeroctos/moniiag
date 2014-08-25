56f427f6d784d1cb5a127d3af1d19e0a81f6e573
Igor Khitrov <ihitrov@atlant-inform.ru>
2014-08-25 17:53:41 +0400

    (!)

--- protected/modules/doctors/views/print/recommendationPdf.php
+++ protected/modules/doctors/views/print/recommendationPdf.php
@@ -8,46 +8,68 @@
 $templatesIndex = $keysOfTemplates[0];
 ?>
 <!-- Название шаблона (берём первый шаблон)-->
-    <h4><?php echo $templates[$templatesIndex ]['name']; ?> от <?php echo $greeting['date']; ?> № <?php echo $greeting['card_number']; ?></h4>
+<h4><?php echo $templates[$templatesIndex ]['name']; ?> от <?php echo $greeting['date']; ?> № <?php echo $greeting['card_number']; ?></h4>
 <!-- Основной диагноз не выводим -->
 <!-- Выводим клинические диагнозы -->
 <?php
+
+if ((count($diagnosises['clinicalSecondary'])>0)||   (strlen($diagnosises['noteGreeting'])>0)  )
+{
+    ?><div><span style="font-size:16px;"><strong>Диагноз</strong></span><?php
     if (count($diagnosises['clinicalSecondary'])>0)
     {
-    ?><div style="margin:0px;"><strong><h3>Диагноз</h3></strong><?php
         foreach ($diagnosises['clinicalSecondary'] as $oneDiagnosis)
         {
             ?><br><strong> - <?php echo $oneDiagnosis['description']; ?></strong><?php
         }
-        ?></div><?php
-    }
-    // Пока я просто оставлю это здесь
-   /* if (strlen($diagnosises['noteGreeting'])>0)
-    {
-        ?><div><strong><h3>Клинические диагноз/диагнозы: </h3></strong><?php
-        echo $diagnosises['noteGreeting'];
-        ?></div><?php
-    }*/
-    // Дальше выводим тело шаблона
-    foreach ($templates as $oneTemplate)
-    {
-
-        foreach($oneTemplate['cats']  as $index => $categorie)
+        if (strlen($diagnosises['noteGreeting'])>0)
         {
-            ?>
-            <div style="margin-left:20px;">
-                <strong style="text-decoration: underline"><?php echo $categorie['element']['name']; ?></strong>
-                <p class ="print-elements">
-                    <?php
-                        // Вызываем виджет категории
-                        $printCategorieWidget = CWidget::createWidget('application.modules.doctors.components.widgets.printCategory', array(
-                            'categoryToPrint' => $categorie
-                        ));
-                        $printCategorieWidget->run();
-                    ?>
-                </p>
-            </div>
-        <?php
+            ?><br><strong><?php echo $diagnosises['noteGreeting']; ?></strong><?php
+        }
+    }
+    ?></div><?php
+}
+
+/*if (count($diagnosises['clinicalSecondary'])>0)
+{
+?><div style="margin:0px;"><strong><h3>Диагноз</h3></strong><?php
+    foreach ($diagnosises['clinicalSecondary'] as $oneDiagnosis)
+    {
+        ?><br><strong> - <?php echo $oneDiagnosis['description']; ?></strong><?php
+    }
+    ?></div><?php
+}*/
+
+
+// Пока я просто оставлю это здесь
+/* if (strlen($diagnosises['noteGreeting'])>0)
+ {
+     ?><div><strong><h3>Клинические диагноз/диагнозы: </h3></strong><?php
+     echo $diagnosises['noteGreeting'];
+     ?></div><?php
+ }*/
+// Дальше выводим тело шаблона
+
+
+foreach ($templates as $oneTemplate)
+{
+
+    foreach($oneTemplate['cats']  as $index => $categorie)
+    {
+        ?>
+        <div style="margin-left:20px;">
+            <strong style="text-decoration: underline"><?php echo $categorie['element']['name']; ?></strong>
+            <p class ="print-elements">
+                <?php
+                // Вызываем виджет категории
+                $printCategorieWidget = CWidget::createWidget('application.modules.doctors.components.widgets.printCategory', array(
+                    'categoryToPrint' => $categorie
+                ));
+                $printCategorieWidget->run();
+                ?>
+            </p>
+        </div>
+    <?php
     }
 }
 ?>
