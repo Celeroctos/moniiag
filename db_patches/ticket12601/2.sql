50df48863790d28779678f54cc296a1e65988e42
unknown <kkosorukov@atlant-inform.ru>
2014-10-08 11:56:18 +0400

    "Q"

--- db_patches/ticket12601/2.sql
+++ db_patches/ticket12601/2.sql
@@ -1,3 +1,19 @@
+/* *
+-- Sequence: mis.medcards_history_seq
+
+-- DROP SEQUENCE mis.medcards_history_seq;
+
+CREATE SEQUENCE mis.medcards_history_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE mis.medcards_history_seq
+  OWNER TO moniiag;
+
+*/
+
 /* INSERT INTO mis.medcards_history 
 	SELECT nextval('mis.medcards_history_seq') as id,  1 as enterprise_id, t.card_number as from, t.card_number as to, t.policy_id as policy_id
 	FROM mis.medcards t */
