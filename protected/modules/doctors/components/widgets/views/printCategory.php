1d585da7696ca3efc46f40f6150e236d1bd28c3f
dmitry <dmitry123@inbox.ru>
2014-10-21 14:45:13 +0400

    Fixes for empty table + optimizations

--- protected/modules/doctors/components/widgets/views/printCategory.php
+++ protected/modules/doctors/components/widgets/views/printCategory.php
@@ -1,150 +1,138 @@
-<?php
-//var_dump("!");
-		//exit();
-		// Перебираем элементы категории - смотрим, если текущий узел дерева - элемент, то выводим его в соответствии с его типом
-		//		Иначе вызываем функцию контроллера вызывающая отрисовку детей
-		//var_dump($category);
-		//exit();
-?>
-<?php
-        $elementNumber = 0;
-		foreach($category as $key => $child) 
-		{	
-			if (!isset ($child['element'])) continue;
-            $elementNumber++;
-			if ($child['element']['element_id']==-1)
-			{
-				// Выводим название категории
-				?>
-				<div style="margin-left:20px;">
-				<strong style="text-decoration: underline"><?php echo $child['element']['name']; ?></strong>
-				<p class ="print-elements">
-				<?php
-                // Вызываем виджет для вложенной категории
-                $printCategorieWidget = CWidget::createWidget('application.modules.doctors.components.widgets.printCategory', array(
-                    'categoryToPrint' => $child
-                ));
-                $printCategorieWidget->run();
+<!-- Look though all categories -->
+<? foreach($category as $key => $child) : ?>
 
-				?>
-                    </p>
-				</div>
-				<?php
-			}
-			else
-			{
-				$element = $child['element'];
-					if ($element['value']!='' && $element['value']!=null)
-					{
-						if ($element['type']=='4')
-						{
-							$configOfTable =  $element['config'];
-							// Редактируемая таблица. Её надо раздербанить по ячейкам и вывести
-							?>
-							<br></br>
-							<table style="border-collapse: collapse;">
-							<tbody>
-							<?php 
-							if(isset($configOfTable['cols']) && count($configOfTable['cols']) > 0) 
-							{
-							?>
-								<tr>
-								<?php
-								if(isset($configOfTable['rows']) && count($configOfTable['rows']) > 0) 
-								{
-								?>
-									<td style="border-width:1px; border-style:solid; border-color:#000000;"></td>
-									<?php
-								}
-								for($i = 0; $i < count($configOfTable['cols']); $i++)
-								{
-								?>
-									<td style="border-width:1px; border-style:solid; border-color:#000000;">
-									<?php
-									echo $configOfTable['cols'][$i];
-									?>
-									</td>
-									<?php
-								   }
-								   ?>
-								</tr>
-								<?php
-							   }
-							   $valuesArr = CJSON::decode($element['value']);
-							   for($i = 0; $i < $configOfTable['numRows']; $i++)
-							   {
-							   ?>
-								<tr>
-								<?php
-								if(isset($configOfTable['rows'][$i]))
-								{
-								?>
-									<td style="border-width:1px; border-style:solid;  border-color:#000000;">
-									<?php 
-									echo $configOfTable['rows'][$i]; 
-									?>
-									</td>
-									<?php
-								   } 
-								   ?>
-								<?php
-								for($j = 0; $j < $configOfTable['numCols']; $j++)
-								{
-								?>
-									<td style="border-width:1px; border-style:solid; border-color:#000000;" class="content-<?php echo $i.'_'.$j; ?>">
-									<?php
-									if(isset($valuesArr[$i][$j]))
-									{
-                            			echo $valuesArr[$i][$j];
-									}
-									?>
-									</td>
-									<?php
-								} 
-								?>
-								</tr>
-								<?php
-							} 
-							?>
-							</tbody>
-							</table>
-							<?php
-						   }
-						   // Всё, что кроме таблицы - выводим значение
-						   else
-						   {
-                               // Если дата - то её нужно перевернуть.
-                               if ($element['type']==6 && $element['value']!='' && $element['value']!=NULL)
-                               {
-                                   $dateParts = array();
-                                   $dateParts = explode('-',$element['value']);
-                                   if (count($dateParts)==3)
-                                   {
-                                        $element['value'] = $dateParts[2].'.'.$dateParts[1].'.'.$dateParts[0];
-                                   }
-                               }
+	<!-- Skip if child havn't element -->
+	<? if (!isset ($child['element'])) {
+		continue;
+	} else {
+		$elementNumber++;
+	} ?>
 
-                                if (($element['is_wrapped']=='1' && $elementNumber!=1)||($ignoreBrSettings==true))
-                                {
+	<!-- If child is category, then print it recursively -->
+	<? if ($child['element']['element_id'] == -1) { ?>
+		<div style="margin-left:20px;">
+		<strong style="text-decoration: underline">
+			<? echo $child['element']['name']; ?>
+		</strong>
+		<p class ="print-elements">
+			<? CWidget::createWidget('application.modules.doctors.components.widgets.printCategory', array(
+				'categoryToPrint' => $child
+			))->run(); ?>
+		</p>
+		</div>
+	<? } else { ?>
 
-                                    ?>
-                                    <br/>
-                                    <?php
+		<!-- Skip print, if elements hasn't value -->
+		<? if (($element = $child['element']) == null || !($element['value'] != ' ' && $element['value'] != null)) {
+			continue;
+		} ?>
 
-                                }
-                               // Если стоит настройка - дополнительно печатаем пустую строку
+		<!-- If element's type is 4 (table), then print table -->
+		<? if ($element['type'] == '4') {
 
-                               if (false)//($ignoreBrSettings==true)
-                                {
-                                    ?>
-                                    <br/>
-                                    <?php
-                                }
+			// Get table's configure
+			$configOfTable =  $element['config'];
 
-                                    ?>
-                                <?php echo $element['label']; ?> <strong><span class="elementValuePrinting"><?php echo $element['value']; ?></span></strong> <?php echo $element['label_after']; ?>
-                                <?php
-						}
+			print "<br>";
+
+			// Set this flag to true, if
+			// you want to render table
+			// with empty values in rows
+			$isRenderEmptyTable = false;
+
+			// Decoded json response from element
+			$valuesArr = CJSON::decode($element['value']);
+
+			// We will print row's title only if we will
+			// have one row in table (cuz we havn't count of
+			// elements in json result, Why?)
+			$isTableTitleRendered = false;
+
+			// Look though all elements in result
+			// and print it
+			for($i = 0; $i < $configOfTable['numRows']; $i++) {
+
+				// Skip not set rows
+				if(isset($configOfTable['rows'][$i]) && !$isRenderEmptyTable) {
+					continue;
+				}
+
+				// We will print column's title only if count
+				// of columns in table greater then 0
+				$isRowTitleRendered = false;
+
+				// Look though all elements in config
+				// table with row's title and it's values
+				for ($j = 0; $j < $configOfTable['numCols']; $j++) :
+
+					// Get value from array or
+					// declare it as null
+					$value = !isset($valuesArr[$i][$j]) ? null
+						: $valuesArr[$i][$j];
+
+					// If fetched value is null, then don't
+					// render it
+					if ($value == null && !$isRenderEmptyTable) {
+						continue;
 					}
+
+					// Render table's header (cuz we havn't
+					// count of rows and columns in config
+					// table, again why?)
+					if (!$isTableTitleRendered && ($isTableTitleRendered = true)) {
+						if (isset($configOfTable['cols']) && count($configOfTable['cols']) > 0) : ?>
+							<table style="border-collapse: collapse;"><tbody>
+							<tr>
+								<?php if (isset($configOfTable['rows']) && count($configOfTable['rows']) > 0) : ?>
+									<td style="border: 1px solid #000000;">
+										<?php echo $configOfTable['rows'][$i]; ?>
+									</td>
+								<?php endif; ?>
+								<?php for ($i = 0; $i < count($configOfTable['cols']); $i++) : ?>
+									<td style="border: 1px solid #000000;">
+										<?php echo $configOfTable['cols'][$i]; ?>
+									</td>
+								<?php endfor; ?>
+							</tr>
+						<?php endif;
+					}
+
+					// Print column's title, only we
+					// have filled values in that row
+					if (!$isRowTitleRendered && ($isRowTitleRendered = true)) : ?>
+						<tr>
+						<td style="border: 1px solid #000000;">
+							<? echo $configOfTable['rows'][$i]; ?>
+						</td>
+					<?php endif; ?>
+					<td style="border: 1px solid #000000;" class="content-<? echo $i . '_' . $j; ?>">
+						<? echo $value;?>
+					</td>
+					</tr>
+				<?php endfor;
+			}
+			if ($isTableTitleRendered) : ?>
+				</tbody></table>
+			<?php endif;
+		} else {
+			// If we have data, then swap it
+			if ($element['type'] == 6 && $element['value'] != '' && $element['value'] != NULL) {
+				if (count(($dateParts = explode('-',$element['value']))) == 3) {
+					$element['value'] = $dateParts[2].'.'.$dateParts[1].'.'.$dateParts[0];
 				}
 			}
+			if (($element['is_wrapped'] == '1' && $elementNumber != 1) || ($ignoreBrSettings == true)) {
+				print "</br>";
+			}
+			print $element['label'];
+			?> <strong>
+				<span class="elementValuePrinting">
+					<?php echo $element['value']; ?>
+				</span>
+			</strong> <?php
+			print $element['label_after']; ?>
+		<?php
+		} ?>
+	<? } ?>
+
+<? endforeach; ?>
