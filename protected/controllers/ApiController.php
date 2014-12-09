25429591d0ac2af6c7e4f7eec1104ce4dafd2a5e
dmitry <dmitry123@inbox.ru>
2014-12-08 18:45:40 -0800

    API

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -12,16 +12,90 @@ class ApiController extends Controller {
      * Register new API 20 bytes SSL key
      */
     public function actionRegister() {
-        if (isset(Yii::app()->user->roleId) && Yii::app()->user->roleId == 1) {
+        if (!isset($_GET['user_id'])) {
             print json_encode(array(
-                "key" => ApiModel::model()->register(),
-                "success" => true
+                "message" => "GET.user_id",
+                "success" => false
             ));
+            return;
+        }
+        $userID = $_GET['user_id'];
+        $userRow = User::model()->getOne($userID);
+        $isSuper = false;
+        foreach (Yii::app()->user->getState('roleId') as $i => $role) {
+            // TODO What the Fuck!!!
+            if ($role['name'] == "Супервайзер") {
+                $isSuper = true;
+                break;
+            }
+        }
+        if ($userRow && $isSuper) {
+            $apiRow = Api::model()->findByUser($userRow['id']);
+            if (count($apiRow) == 0) {
+                print json_encode(array(
+                    "key" => Api::model()->register($userRow['id']),
+                    "success" => true
+                ));
+            } else {
+                print json_encode(array(
+                    "key" => $apiRow[0]["key"],
+                    "success" => true
+                ));
+            }
         } else {
             print json_encode(array(
                 "message" => "Недостаточно прав",
-                "success" => false,
-                "role" => Yii::app()->user
+                "success" => false
+            ));
+        }
+    }
+
+    /**
+     * Validate API and open session
+     */
+    public function actionValidate() {
+        // Check parameters
+        if (!isset($_GET['key']) || !isset($_GET['login']) || !isset($_GET['password'])) {
+            print json_encode(array(
+                "message" => "GET.key, GET.login, GET.password",
+                "success" => false
+            ));
+            return;
+        }
+        // Get parameters
+        $key = $_GET['key'];
+        $password = $_GET['password'];
+        $login = $_GET['login'];
+        // Fetch API by user's login
+        $apiRow = Api::model()->findByUserLogin($login);
+        // Check row and compare it's keys
+        if (!$apiRow || $apiRow[0]['key'] !== $key) {
+            print print_r(array(
+                "message" => "Invalid login or password or key",
+                "success" => false
+            ));
+        }
+        // Identify user
+        $userIdent = new UserIdentity($login, $password);
+        // First step with skipped second step
+        if($userIdent->authenticateStep1(true)) {
+            Yii::app()->user->login($userIdent);
+            echo CJSON::encode(array(
+                'session' => Yii::app()->getSession()->getSessionId(),
+                'success' => 'true'
+            ));
+            exit();
+        } else {
+            $resultCode = 'loginError';
+            if ($userIdent->wrongLogin()) {
+                $resultCode = 'notFoundLogin';
+            }
+            if ($userIdent->wrongPassword()) {
+                $resultCode = 'wrongPassword';
+            }
+            echo CJSON::encode(array(
+                'success' => $resultCode,
+                'errors' => $userIdent->errorMessage
             ));
         }
     }
--- protected/models/Api.php
+++ protected/models/Api.php
@@ -6,7 +6,15 @@
  * Time: 16:07
  */
 
-class Api extends CModel {
+class Api extends MisActiveRecord {
+
+    /**
+     * @param string $className
+     * @return mixed
+     */
+    public static function model($className=__CLASS__) {
+        return parent::model($className);
+    }
 
     /**
      * @return string - Table's name
@@ -19,15 +27,62 @@ class Api extends CModel {
      * @param $key string - 20 bytes random SSL key
      * @return array|null - Found api row in table
      */
-    public function get($key) {
+    public function findByKey($key) {
         try {
             return Yii::app()->db->createCommand()
                 ->select("*")
                 ->from("mis.api")
-                ->where("key = :key")
-                ->queryRow(array(
+                ->where("key = :key", array(
                     ":key" => $key
-                ));
+                ))
+                ->queryAll();
+        } catch (Exception $e) {
+            print json_encode(array(
+                'message' => $e->getMessage(),
+                'success' => false
+            )); die;
+        }
+        return null;
+    }
+
+    /**
+     * Find by user's identifier
+     * @param $userID int - User's identifier
+     * @return array - With row
+     */
+    public function findByUser($userID) {
+        try {
+            return Yii::app()->db->createCommand()
+                ->select("*")
+                ->from("mis.api")
+                ->where("user_id = :id", array(
+                    ":id" => $userID
+                ))
+                ->queryAll();
+        } catch (Exception $e) {
+            print json_encode(array(
+                'message' => $e->getMessage(),
+                'success' => false
+            )); die;
+        }
+        return null;
+    }
+
+    /**
+     * Find by user's identifier
+     * @param $login string - User's login
+     * @return array - With row
+     */
+    public function findByUserLogin($login) {
+        try {
+            return Yii::app()->db->createCommand()
+                ->select("*")
+                ->from("mis.api a")
+                ->join("mis.users u", "a.user_id = u.id")
+                ->where("u.login = :login", array(
+                    ":login" => $login
+                ))
+                ->queryAll();
         } catch (Exception $e) {
             print json_encode(array(
                 'message' => $e->getMessage(),
@@ -41,14 +96,15 @@ class Api extends CModel {
      * Register new key in API table
      * @return array - Registered row
      */
-    public function register() {
+    public function register($userID) {
         try {
-            $key = openssl_random_pseudo_bytes(20);
+            $key = bin2hex(openssl_random_pseudo_bytes(20));
             Yii::app()->db->createCommand()
                 ->insert("mis.api", array(
-                    "key" => $key
+                    "key" => $key,
+                    "user_id" => $userID
                 ));
-            return $this->get($key);
+            return $key;
         } catch (Exception $e) {
             print json_encode(array(
                 'message' => $e->getMessage(),
