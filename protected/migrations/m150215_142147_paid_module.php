ba139b9e27a2357400b92663fc9111fb3154fa22
Dzhamal <prohps@yandex.ru>
2015-02-16 12:37:01 +0300

    continue create struct Datebase

--- protected/migrations/m150215_142147_paid_module.php
+++ protected/migrations/m150215_142147_paid_module.php
@@ -19,7 +19,7 @@ class m150215_142147_paid_module extends CDbMigration
                         "name" character varying(255) NOT NULL, --Имя группы
                         "p_id" integer DEFAULT NULL, --Родитель группы, NULL, если нету
                         PRIMARY KEY (id_paid_group)
-                    );      
+                    );
 HERE;
             $command=$connection->createCommand($sql);
             $command->execute();
@@ -53,18 +53,22 @@ HERE;
 					(
 						"id_paid_order" serial NOT NULL,
 						"name" character varying(255),
+						"id_user_create" integer NOT NULL, --Пользователь, создавший заказ
+						"id_paid_expense" integer, --Номер счета, при статусе "новое" пустое значение, при статусе "включено в счет" ID счета					
+						"status" integer, --Оплачен/не оплачен (1/0)
 						PRIMARY KEY(order_id)
 					);
 HERE;
 			$command=$connection->createCommand($sql);
 			$command->execute();
-			
+			/*Таблица на самом деле является TEMP-хранилищем для создания направлений на её основе, можно чистить.*/
 			$sql=<<<HERE
 					CREATE TABLE IF NOT EXISTS "paid_order_details"
 					(
 						"id_paid_order_detail" serial NOT NULL,
 						"id_paid_order" integer NOT NULL, --FK (table paid_orders)
 						"id_paid_service" integer NOT NULL, --FK (table paid_services)
+						PRIMARY KEY(id_paid_order_detail)
 					);
 HERE;
 			$command=$connection->createCommand($sql);
@@ -74,13 +78,50 @@ HERE;
 					CREATE TABLE IF NOT EXISTS "paid_referrals"
 					(
 						"id_paid_referrals" serial NOT NULL, --Уникальный номер направления
-						"id_paid_order" integer NOT NULL, --FK (table paid_order_details)
+						"id_paid_order" integer NOT NULL, --FK (table paid_orders)
+						"id_medcard" integer NOT NULL, --FK (table medcards)
 						"date" TIMESTAMPTZ,
-						"status" character varying(255), --Новое/включено в счет (0/1)
-						"user_create_id" --Пользователь, создавший заказ
-						
-					)
+						"status" integer, --Сомнительно, возможно удаление (есть в paid_orders)
+						PRIMARY KEY(id_paid_referrals)
+					);
 HERE;
+			$command=$connecction->createCommand($sql);
+			$command->execute();
+			
+			$sql=<<<HERE
+					CREATE TABLE IF NOT EXISTS "paid_referrals_details"
+					(
+						"id_paid_referral_detail" serial NOT NULL,
+						"id_paid_service" integer NOT NULL,
+						"id_paid_referral" integer NOT NULL,
+						PRIMARY KEY(id_paid_referral_detail)
+					);
+HERE;
+			$command=$connection->createCommand($sql);
+			$command->execute();
+			
+			$sql=<<<HERE
+					CREATE TABLE IF NOT EXISTS "paid_expenses"
+					(
+						"id_paid_expense" serial NOT NULL,
+						"date" TIMESTAMPTZ,
+						"id_paid_order" integer NOT NULL, --FK (table paid_orders)
+						"status" integer, --Сомнительно, возможно удаление (есть в paid_orders)
+						PRIMARY KEY(id_paid_expense)
+					);
+HERE;
+			$command=$connection->createCommand($sql);
+			$command->execute();
+			
+			$sql=<<<HERE
+					CREATE TABLE IF NOT EXISTS "paid_payments"
+					(
+						"id_paid_payment" serial NOT NULL,
+						"id_paid_expense" integer NOT NULL, --FK (table paid_expenses)
+						
+					);
+HERE;
+			
             $transaction->commit();
             }
 		catch(Exception $e)
