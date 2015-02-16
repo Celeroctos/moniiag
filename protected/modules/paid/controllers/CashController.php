7baebc2a506a31bbdd9bda644a564a628e6eee03
Dzhamal <prohps@yandex.ru>
2015-02-16 18:55:26 +0300

    мелочь

---
+++ protected/modules/paid/controllers/CashController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Контроллер для работы кассы
+ * @author Dzhamal Tayibov <prohps@yandex.ru>
+ */
+class CashController extends Controller
+{
+	public function actionIndex()
+	{
+		
+	}
+}
